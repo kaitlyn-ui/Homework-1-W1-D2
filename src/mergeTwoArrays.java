6b36f29a832b2bf296311f3c695e428fc3b1f6df
kaitlyn-ui <kaitlyn.k.luu@sjsu.edu>
2026-02-05 18:14:31 -0800

    Update mergeTwoArrays.java

--- src/mergeTwoArrays.java
+++ src/mergeTwoArrays.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Arrays;
 
 public class mergeTwoArrays {
 
     public static int[] merge(int[] array1, int[] array2) {
 
+        //variables
         int[] tempArray = new int[array1.length + array2.length];
         int i = 0, j = 0, k = 0;
 
 
+        //loop until indexes j,k reaches end of one or both arrays 
         while (j < array1.length && k < array2.length) {
             if (array1[j] < array2[k]) {
                 tempArray[i] = array1[j];
@@ -21,18 +21,23 @@ public class mergeTwoArrays {
                 k++;
             }
         }
+
+        //cases for dealing with two different length arrays
+
+        //if array1 has leftover elements when first loop ends, add rest of elements to end of array
         while (j < array1.length) {
             tempArray[i] = array1[j];
             i++;
             j++;
         }
 
+        //if array2 has leftover elements when first loop ends, add rest of elements to end of array
         while (k < array2.length) {
             tempArray[i] = array2[k];
             i++;
             k++;
         }
-        return tempArray;
+        return tempArray; //return merged, sorted array
     }
 }
 
